8b8744edda6559d6a817f72ce399a08218111d3a
andresszzzzzz <javierortegatobar7@gmail.com>
2026-09-11 16:06:17 -0500

    Arreglo de reglas de calificaciones

--- backend/src/routes/calificacion.routes.js
+++ backend/src/routes/calificacion.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { verificarToken } = require("../middlewares/auth");
+const { permitirRoles } = require("../middlewares/roleAuth");
 
 const {
   obtenerCalificaciones,
@@ -8,15 +9,23 @@ const {
   crearCalificacion,
   actualizarCalificacion,
   eliminarCalificacion,
+  registrarRecuperacion,
+  registrarHabilitacion,
 } = require('../controllers/calificacion.controller');
 
 router.use(verificarToken);
 
+const puedeCalificar = permitirRoles('docente', 'admin', 'rector', 'coordinador');
+
 // Endpoints de Calificaciones
 router.get('/', obtenerCalificaciones);
 router.get('/:id', obtenerCalificacionPorId);
-router.post('/', crearCalificacion);
-router.put('/:id', actualizarCalificacion);
-router.delete('/:id', eliminarCalificacion);
+router.post('/', puedeCalificar, crearCalificacion);
+router.put('/:id', puedeCalificar, actualizarCalificacion);
+router.delete('/:id', puedeCalificar, eliminarCalificacion);
+
+// Recuperaciones y habilitaciones (mismos roles que pueden calificar)
+router.put('/:id/recuperacion', puedeCalificar, registrarRecuperacion);
+router.put('/:id/habilitacion', puedeCalificar, registrarHabilitacion);
 
 module.exports = router;
